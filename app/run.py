151ffafc8635e6cd535ce8f233d4a6d436b61099
nagamani <nagamanicg229@gmail.com>
2025-09-17 16:01:38 -0400

    Changes wrt the grades reading from the file and using pydantic library

--- app/run.py
+++ app/run.py
@@ -7,11 +7,11 @@ from grade_calculator import GradeCalculator
 # This runs the grade calculation.
 
 # Instatiate Grade and Weights objects
-my_grades = Grades()
+my_grades=Grades.from_json_file('grades.json')
 weights = GradeWeights()
 
 # Set grades achieved so far
-my_grades.quiz_1 = 0.78 # Received 78% in the first quiz
+# my_grades.quiz_1 = 0.78 # Received 78% in the first quiz
 
 # Print out the grades to console
 print(my_grades)
